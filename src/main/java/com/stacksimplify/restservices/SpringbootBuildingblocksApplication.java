221d1b71e9ebe526b50f5b470555bd2f0400aa53
Krishnaduth <krishnaduth777@gmail.com>
2024-07-24 04:41:45 +0530

    First Commit

---
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -0,0 +1,23 @@
+package com.stacksimplify.restservices;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringbootBuildingblocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildingblocksApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(){
+		return runner ->{
+			System.out.println("Hello World");
+		};
+	}
+
+	
+}
